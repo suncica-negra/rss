648f49ff6d504530165d0d420ea6491cdddd0428
suncica-negra <suncica.negra@gmail.com>
2021-12-27 21:26:52 +0100

    Create a Nuxt.js application

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    New empty repository where a great RSS app for "Styria Digital Development"
+    company will be created!
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
