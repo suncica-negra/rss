d795b0aa5b21b0b2e7a701c343c20a26fde4df51
suncica-negra <suncica.negra@gmail.com>
2021-12-27 23:06:44 +0100

    Styled footer component

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,14 @@
 <template>
-  <div>
-    FOOTER
+  <div id="footer">
+    <div class="copy">
+      <a
+        class="copy transition"
+        href="https://www.linkedin.com/in/dea-k-331534187/"
+        target="_blank"
+        >okrasnic</a
+      >
+      &copy; {{ new Date().getFullYear() }}.
+    </div>
   </div>
 </template>
 
@@ -9,3 +17,25 @@ export default {
   name: "Footer",
 };
 </script>
+
+<style lang="scss">
+#footer {
+  width: 100%;
+  padding: 10px 0;
+  box-shadow: 0 0 7px var(--box-shadow-transparent);
+
+  .copy {
+    text-align: center;
+    font-size: 13px;
+    color: var(--color);
+
+    a {
+      letter-spacing: 2px;
+
+      &:hover {
+        color: var(--color-primary);
+      }
+    }
+  }
+}
+</style>
--- layouts/default.vue
+++ layouts/default.vue
@@ -18,7 +18,7 @@ export default {
 };
 </script>
 
-<style>
+<style lang="scss">
 #body {
   min-height: 100vh;
 }
