9e111ae7c52b14b179eba000cf6abbea4202ec5d
suncica-negra <suncica.negra@gmail.com>
2021-12-28 19:56:22 +0100

    Define routes

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,6 @@
 
 <script>
 export default {
-  name: "IndexPage",
+  name: "Landing",
 };
 </script>
---
+++ pages/show/index.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    Show page
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Show",
+};
+</script>
