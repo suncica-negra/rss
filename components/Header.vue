3fad266566bcb4839a98fe37b4f54790ac3a2de3
suncica-negra <suncica.negra@gmail.com>
2021-12-28 20:22:42 +0100

    Add routes to the header

--- components/Header.vue
+++ components/Header.vue
@@ -1,11 +1,26 @@
 <template>
   <div>
-    HEADER
+    <div v-for="(link, i) in linksToPages" :key="i">
+      <nuxt-link :to="'/' + link">{{ link.toUpperCase() }}</nuxt-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "Header",
+  data() {
+    return {
+      linksToPages: [
+        "fun",
+        "lifestyle",
+        "latest",
+        "news",
+        "show",
+        "sport",
+        "tech",
+      ],
+    };
+  },
 };
 </script>
