63bcd002be9c140d6260d72b7ce9b046e10ee742
suncica-negra <suncica.negra@gmail.com>
2022-01-01 19:31:11 +0100

    Assemble the latest page

--- pages/latest.vue
+++ pages/latest.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    Latest page
+    <Title :title="title" />
+    <Spinner v-show="!feedsData" />
+    <ArticleSummaryBox v-if="feedsData" :feedsData="feedsData" />
   </div>
 </template>
 
 <script>
+import Title from "../components/Title.vue";
+import Spinner from "../components/Spinner.vue";
+import { getDataFromFeed } from "../helpers/apiReader";
+import ArticleSummaryBox from "../components/ArticleSummaryBox.vue";
+
 export default {
+  components: { Title, Spinner, ArticleSummaryBox },
   name: "Latest",
+  data() {
+    return {
+      feedsData: null,
+      title: "Latest",
+    };
+  },
+  created() {
+    this.getArticlesData();
+  },
+  methods: {
+    getArticlesData() {
+      var that = this;
+      this.feedsData = getDataFromFeed.readFromAPI(
+        "https://www.24sata.hr/feeds/najnovije.xml",
+        that
+      );
+    },
+  },
 };
 </script>
