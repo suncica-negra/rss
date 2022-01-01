9858c040f2f69f288d3dee58b4db78ad84c5db80
suncica-negra <suncica.negra@gmail.com>
2022-01-01 19:30:30 +0100

    Assemble the fun page

--- pages/fun.vue
+++ pages/fun.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    Fun page
+    <Title :title="title" />
+    <Spinner v-show="!feedsData" />
+    <ArticleSummaryBox v-if="feedsData" :feedsData="feedsData" />
   </div>
 </template>
 
 <script>
+import Title from "../components/Title.vue";
+import Spinner from "../components/Spinner.vue";
+import { getDataFromFeed } from "../helpers/apiReader";
+import ArticleSummaryBox from "../components/ArticleSummaryBox.vue";
+
 export default {
+  components: { Title, Spinner, ArticleSummaryBox },
   name: "Fun",
+  data() {
+    return {
+      feedsData: null,
+      title: "Fun",
+    };
+  },
+  created() {
+    this.getArticlesData();
+  },
+  methods: {
+    getArticlesData() {
+      var that = this;
+      this.feedsData = getDataFromFeed.readFromAPI(
+        "https://www.24sata.hr/feeds/fun.xml",
+        that
+      );
+    },
+  },
 };
 </script>
