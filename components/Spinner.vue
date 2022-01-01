723cec80cd898fa3bc5c121221dfbf8c91197373
suncica-negra <suncica.negra@gmail.com>
2022-01-01 17:43:27 +0100

    Create a component for the spinner

---
+++ components/Spinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="spinner">
+    <img src="~/assets/images/spinner.gif" alt="Spinner" />
+    <p>Pričekajte....</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Spinner",
+};
+</script>
+
+<style lang="scss">
+.spinner {
+  position: relative;
+  text-align: center;
+  min-height: 204px;
+
+  p {
+    margin: auto;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    height: 23px;
+    color: var(--aqua);
+  }
+}
+</style>
