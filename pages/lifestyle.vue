7634c37a3ebd35c7e7254383939313ce696cae12
suncica-negra <suncica.negra@gmail.com>
2022-01-01 19:31:50 +0100

    Assemble the lifestyle page

--- pages/lifestyle.vue
+++ pages/lifestyle.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
-    Lifestyle page
+    <Title :title="title" />
+    <Spinner v-show="!feedsData" />
+    <ArticleSummaryBox v-if="feedsData" :feedsData="feedsData" />
   </div>
 </template>
 
 <script>
+import Title from "../components/Title.vue";
+import Spinner from "../components/Spinner.vue";
+import { getDataFromFeed } from "../helpers/apiReader";
+import ArticleSummaryBox from "../components/ArticleSummaryBox.vue";
+
 export default {
+  components: { Title, Spinner, ArticleSummaryBox },
   name: "Lifestyle",
+  data() {
+    return {
+      feedsData: null,
+      title: "Lifestyle",
+    };
+  },
+  created() {
+    this.getArticlesData();
+  },
+  methods: {
+    getArticlesData() {
+      var that = this;
+      this.feedsData = getDataFromFeed.readFromAPI(
+        "https://www.24sata.hr/feeds/lifestyle.xml",
+        that
+      );
+    },
+  },
 };
 </script>
