fd97cd49c3b027d3be44a669427b8dc3e009547e
suncica-negra <suncica.negra@gmail.com>
2022-01-01 17:35:21 +0100

    Create a component for the title

---
+++ components/Title.vue
@@ -0,0 +1,27 @@
+<template>
+  <h2>
+    {{ title }}
+    <span class="material-icons">rss_feed</span>
+  </h2>
+</template>
+
+<script>
+export default {
+  name: "Title",
+  props: {
+    title: {
+      type: String,
+      default: "",
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+h2 {
+  margin: 30px;
+  font-size: 40px;
+  display: flex;
+}
+</style>
