66a670116175d3690ace2e2049035753b0d7a4a2
suncica-negra <suncica.negra@gmail.com>
2022-01-03 19:05:00 +0100

    Add the ability to view the demo version (hard-coded .xml file) if there is no access to the RSS service

--- helpers/apiReader.js
+++ helpers/apiReader.js
@@ -1,3 +1,6 @@
+import Swal from "sweetalert2";
+import { XML } from "./xmlfile.js";
+
 export const getDataFromFeed = {
   readFromAPI(apiUrl, that) {
     const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
@@ -9,8 +12,12 @@ export const getDataFromFeed = {
       })
       .catch((error) => {
         console.log(error);
-        that.error = true;
-        that.loading = false;
+        if (error.message === "Network Error" || error.message === "Request failed with status code 403") {
+          swal.swalErrorResponseHandler(error.message, that)
+        } else {
+          that.error = true;
+          that.loading = false;
+        }
       });
   }
 }
@@ -29,6 +36,40 @@ export const parseFetchedData = {
           that.error = false,
           that.loading = false
         );
+      }
+    });
+  }
+}
+
+export const swal = {
+  swalErrorResponseHandler(error, that) {
+    Swal.fire({
+      icon: "info",
+      title: error,
+      showDenyButton: true,
+      showCancelButton: true,
+      confirmButtonText: 'Imam dopuštenje s gornjeg linka',
+      denyButtonText: `Ne, prikaži demo`,
+      cancelButtonText: 'Odustani',
+      html: `<p style="font-family:Quicksand,sans-serif;">
+        Da bi mogli čitati s 24sata RSS servisa morate imati dopuštenje. Da bi dobili dopuštenje odaberite 
+        <b style="display:block;">Da, idi i potvrdi pristup</b> Na stranici koja vam se otvori kliknite na 
+        <b style="display:block;">Request temporary access to the demo server</b> 
+        Zatim se vratite ovdje i odaberite <b style="display:block;">Imam dopuštenje s gornjeg linka</b>.
+        </p><button type="button" class="swal2-confirm swal2-styled" style="display:inline-block;">
+        <a href="https://cors-anywhere.herokuapp.com/corsdemo" target="_blank" 
+        style="color:white;font-family:Quicksand,sans-serif;">Da, idi i potvrdi pristup</a></button>`
+    }).then((result) => {
+      if (result.isConfirmed) {
+        location.reload();
+      } else if (result.isDenied) {
+        that.error = false;
+        that.loading = false;
+        parseFetchedData.getData(XML, that);
+        Swal.fire('Prikazuju se stare vijesti', '', 'info')
+      } else {
+        that.error = true;
+        that.loading = false;
       }
     });
   }
